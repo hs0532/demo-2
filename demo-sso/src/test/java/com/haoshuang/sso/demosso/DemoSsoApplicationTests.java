4aec0f21e0bbfb5be0eb7198bcc940b831efb9b5
hs0532 <a554042469@163.com>
2020-10-12 11:49:29 +0800

    初始化

---
+++ demo-sso/src/test/java/com/haoshuang/sso/demosso/DemoSsoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haoshuang.sso.demosso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSsoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
