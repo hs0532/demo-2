9bcb96e33dd445ecb8c49fe20984c8fe1c23114a
hs0532 <a554042469@163.com>
2020-10-23 16:21:32 +0800

    222

--- demo-sso/src/main/java/com/haoshuang/sso/demosso/config/MyFailAuthenticationFailHandler.java
+++ demo-sso/src/main/java/com/haoshuang/sso/demosso/config/MyFailAuthenticationFailHandler.java
@@ -8,7 +8,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.*;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
 
@@ -17,6 +19,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Date;
 
 @Component
 @Slf4j
@@ -38,9 +41,27 @@ public class MyFailAuthenticationFailHandler implements AuthenticationFailureHan
         httpServletResponse.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
         httpServletResponse.setContentType("application/json;charset=UTF-8");
         httpServletResponse.setStatus(200);
+        String reson = null;
+        if (e instanceof BadCredentialsException ||
+                e instanceof UsernameNotFoundException) {
+            reson="账户名或者密码输入错误!";
+        } else if (e instanceof LockedException) {
+            reson="账户被锁定，请联系管理员!";
+        } else if (e instanceof CredentialsExpiredException) {
+            reson="密码过期，请联系管理员!";
+        } else if (e instanceof AccountExpiredException) {
+            reson="账户过期，请联系管理员!";
+        } else if (e instanceof DisabledException) {
+            reson="账户被禁用，请联系管理员!";
+        } else {
+            reson="其他未知原因原因!";
+            log.info("用户登录失败，失败原因{}",e.toString());
+        }
+
+        httpServletResponse.setStatus(200);
         ResultBean resultBean = new ResultBean();
         resultBean.setCade(1);
-        resultBean.setMsg(e.getMessage());
+        resultBean.setMsg(reson);
         PrintWriter out =  httpServletResponse.getWriter();
         //.write(objectMapper.writeValueAsString(e));
         ObjectMapper objectMapper = new ObjectMapper();
