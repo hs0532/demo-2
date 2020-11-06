2fb80e819a1dd9a8cbb58a4465e5d5c8964c4cab
hs0532 <a554042469@163.com>
2020-11-06 11:47:50 +0800

    修改前缀 /api

--- demo-sso/src/main/java/com/haoshuang/sso/demosso/config/SecurityConfig.java
+++ demo-sso/src/main/java/com/haoshuang/sso/demosso/config/SecurityConfig.java
@@ -30,7 +30,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/code/image");
+        web.ignoring().antMatchers("/api/code/image");
     }
 
     @Override
@@ -43,7 +43,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .failureHandler(myFailAuthenticationFailHandler)
                 .and()
                 .authorizeRequests()
-                .antMatchers("/login","/register","/static/**","/css/**","/code/image","/check/*").permitAll()//过滤不需要拦截认证的资源
+                .antMatchers("/login","/register","/static/**","/css/**","/code/image","/api/check/*").permitAll()//过滤不需要拦截认证的资源
                 .anyRequest()
                 .authenticated()
                 .and()
