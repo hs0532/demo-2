07910cc801fb435c8c352ada58474f6c456b8e22
hs0532 <a554042469@163.com>
2020-11-11 09:58:56 +0800

    测试

--- demo-sso/src/test/java/com/haoshuang/sso/demosso/common/validate/validateInterface/SlipImageValidateCodeGeneratorTest.java
+++ demo-sso/src/test/java/com/haoshuang/sso/demosso/common/validate/validateInterface/SlipImageValidateCodeGeneratorTest.java
@@ -4,13 +4,13 @@ import com.haoshuang.sso.demosso.common.validate.ValidateCodeBean.VerifyImageCod
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import sun.jvm.hotspot.utilities.Assert;
+//import sun.jvm.hotspot.utilities.Assert;
 
 import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
+//@SpringBootTest
 class SlipImageValidateCodeGeneratorTest {
 
     @Autowired
